d8f8d59510905ff8c424a3ad53c3e3e5b0bbc946
Syam <syamsundarpinakkattu@gmail.com>
2024-02-25 20:51:06 +0530

    Registration page halfway

--- client/blockchainphr/src/components/signup.js
+++ client/blockchainphr/src/components/signup.js
@@ -2,9 +2,29 @@ import React from 'react';
 
 const Signup = () => {
   return (
-    <div className="signup">
-      <h2>Sign Up</h2>
-      {/* Add your sign up form here */}
+    <div className="container">
+      <header>Registration</header>
+      <form action="#">
+        <div class="form-first">
+          <div class="personal-details">
+            <span class="title">Personal Details</span>
+            <div class="fields">
+              <div class="input-field">
+                <label>Full Name</label>
+                <input type="text" placeholder="Enter your Name" required></input>
+              </div>
+              <div class="input-field">
+                <label>Date of Birth</label>
+                <input type="date"  required></input>
+              </div>
+              <div class="input-field">
+                <label>Email</label>
+                <input type="text" placeholder="Enter your Mail" required></input>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };

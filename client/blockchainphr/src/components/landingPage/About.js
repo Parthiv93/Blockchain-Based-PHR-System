7b5ca4b0d939dccb4cc5b46cb1c53b6cb57ed3df
sreejesh46 <86306486+sreejesh46@users.noreply.github.com>
2024-02-28 09:47:46 +0530

    changes

--- client/blockchainphr/src/components/landingPage/About.js
+++ client/blockchainphr/src/components/landingPage/About.js
@@ -1,13 +1,6 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-import "./styles.css"
-import name from "../../assets/img/dashboard/admin-user.png";
-import email from "../../assets/img/dashboard/admin-email.png";
-import admin_1 from "../../assets/img/dashboard/admin-1.jpg";
-import admin_git from "../../assets/img/dashboard/admin-git.png";
-import admin_insta from "../../assets/img/dashboard/admin-insta-2.png";
-import admin_linkedin from "../../assets/img/dashboard/admin-linkedin.png";
-import admin_card_profile from "../../assets/img/dashboard/admin-card-profile.png";
+import "./styles.css";
 const About = () => {
   return (
     <div className="flex flex-col h-screen body lg:overflow-hidden lg:h-screen max-h-min">

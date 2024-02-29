65824073804ee92cae7280c3a854db9f11d82c48
sreejesh46 <86306486+sreejesh46@users.noreply.github.com>
2024-03-01 01:13:59 +0530

    Updated About Us section

--- client/blockchainphr/src/components/landingPage/About.js
+++ client/blockchainphr/src/components/landingPage/About.js
@@ -2,11 +2,11 @@ import Footer from "./Footer";
 import Navbar from "./Navbar";
 const About = () => {
   return (
-    <div className="body  lg:overflow-hidden lg:h-screen max-h-min flex flex-col h-screen">
+    <div className="flex flex-col h-screen body lg:overflow-hidden lg:h-screen max-h-min">
       <Navbar></Navbar>
       <div className="p-9">
         <div className="flex justify-center">
-          <h1 className="ml-2">
+          <h1 className="max-w-full px-16 py-8 mx-4 my-8 ml-2 font-medium bg-white border-8 font-poppins lg:mx-32 rounded-3xl border-primary">
             Welcome to Care Cryption, <br />
             Your blockchain-based health record system.<br />
             Log in securely as a patient or doctor to access medical records.<br />
